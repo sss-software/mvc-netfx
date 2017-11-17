af8866f984fa4ed9b8d6a19ac1cd501b41d8b115
Demis Bellot <demis.bellot@gmail.com>
2017-11-17 02:47:50 -0500

    Add MVC project

---
+++ MyApp/AppHost.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+using Funq;
+using ServiceStack;
+using ServiceStack.Mvc;
+using MyApp.ServiceInterface;
+
+
+namespace MyApp
+{
+    public class AppHost : AppHostBase
+    {
+        /// <summary>
+        /// Base constructor requires a Name and Assembly where web service implementation is located
+        /// </summary>
+        public AppHost()
+            : base("MyApp", typeof(MyServices).Assembly) { }
+
+        /// <summary>
+        /// Application specific configuration
+        /// This method should initialize any IoC resources utilized by your web service classes.
+        /// </summary>
+        public override void Configure(Container container)
+        {
+            SetConfig(new HostConfig
+            {
+                HandlerFactoryPath = "api",
+            });
+            //Config examples
+            //this.Plugins.Add(new PostmanFeature());
+            //this.Plugins.Add(new CorsFeature());
+
+            //Set MVC to use the same Funq IOC as ServiceStack
+            ControllerBuilder.Current.SetControllerFactory(new FunqControllerFactory(container));
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# web-netfx
+
+ServiceStack ASP.NET v4.5 Empty Web project
